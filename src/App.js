5ac031396449c4d54e6da2b819b5b5cfdebbaebe
Arvinder Singh <sardarchitect@gmail.com>
2020-06-22 13:47:39 -0400

    added q/a section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Text } from "./Text";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1> Seek2Solve </h1>
+          <h2> An app to change it all </h2>
+        </div>
       </header>
+
+      <div className = "QA">
+        {Text.map(para => para.includes("Q.") ? <strong> {para} </strong> : <p> {para} </p>)}
+      </div>
     </div>
   );
 }
